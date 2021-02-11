d6f0b1f3a2e69b935160b654c5bfca1d34469b85
Hydrochlorick <hydrochlorick@icloud.com>
2021-02-10 17:16:46 -0800

    WE DID IT REDDIT

---
+++ Bot Shop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bot Shop
+//
+//  Created by Rick Jacobson on 2/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+
+}
+
