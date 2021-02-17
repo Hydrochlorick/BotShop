edb6fa17d64559d2cb2c50a7886b025df90de246
Hydrochlorick <hydrochlorick@icloud.com>
2021-02-16 16:11:00 -0800

    Attempted to set up collection view and custom cells, but not successfully doing so yet.

---
+++ Bot Shop/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Bot Shop
+//
+//  Created by Rick Jacobson on 2/14/21.
+//
+
+import Foundation
+import UIKit
+
+struct Item {
+    var title: String
+    var image: UIImage
+}
--- Bot Shop/NewOrderViewController.swift
+++ Bot Shop/NewOrderViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class NewOrderViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class NewOrderViewController: UIViewController {
     
     let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -20,22 +20,6 @@ class NewOrderViewController: UIViewController, UICollectionViewDelegate, UIColl
         
         return collectionView
     }()
-    
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! NewItemCell
-        cell.backgroundColor = .green
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width/2.2, height: collectionView.frame.height/3)
-    }
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,3 +44,22 @@ class NewOrderViewController: UIViewController, UICollectionViewDelegate, UIColl
 
 }
 
+extension NewOrderViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! NewItemCell
+        cell.backgroundColor = .green
+        return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/2.2, height: collectionView.frame.height/3)
+    }
+    
+//    func collectionView
+
+}
